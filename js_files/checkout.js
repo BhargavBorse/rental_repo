8665acf8ec03708278078a8c7b2ace889179de45
Bhargav Borse <bhargavborse4898@gmail.com>
2019-01-31 12:35:48 +0530

    checkout page done

--- js_files/checkout.js
+++ js_files/checkout.js
@@ -1,20 +1,48 @@
 var eventRef = firebase.database().ref('users');
- 
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
         // console.log(user.uid);
         console.log(user.email);
-        document.getElementById('user_email').value = user.email;
-        document.getElementById('user_email_text').value = user.email;
-      
+        document.getElementById('user_email').innerHTML = user.email;
+        document.getElementById('user_email_text').innerHTML = user.email;
+        
+        // this part is for auto fill. will be used in personal details
+        eventRef.child(user.uid).child('details').on('value',function(user_details_snapshot){
+            var user_details = user_details_snapshot.val();
+            document.getElementById('name').value = user_details.name;
+            document.getElementById('l_name').value = user_details.l_name;
+            document.getElementById('address').value = user_details.Address;
+            document.getElementById('pin_code').value = user_details.pin_code;
+            document.getElementById('phone_no').value = user_details.phone_number;
+        });
+        // end of fetching personal details
+
+        document.getElementById('wd_form_submit').onclick = function(){
+            var name_wd = document.getElementById('name').value;
+            var l_name_wd = document.getElementById('l_name').value;
+            var phn_wd = document.getElementById('phone_no').value;
+            var address_wd = document.getElementById('address').value;
+            var pincode_wd = document.getElementById('pin_code').value;
+            
+            eventRef.child(user.uid).child('details').update({
+                name: name_wd, 
+                l_name: l_name_wd,
+                phone_number: phn_wd,
+                Address: address_wd,
+                pin_code: pincode_wd
+            });
+            // window.location = 'index.html';
+        };
+        
     } else {
         // No user is signed in.
         window.alert('Sorry! No user has been signed in. Please try logging in again');
         window.location = 'Guest index.html';
     }
 });
- 
+
 function logout_user(){
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
@@ -24,5 +52,5 @@ function logout_user(){
         // An error happened.
         alert('Please try again');
     });
-   
+    
 }
--- js_files/welcome-details.js
+++ js_files/welcome-details.js
@@ -14,6 +14,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         eventRef.child(user.uid).child('details').on('value',function(user_details_snapshot){
             var user_details = user_details_snapshot.val();
             document.getElementById('name').value = user_details.name;
+            document.getElementById('l_name').value = user_details.l_name;
             document.getElementById('address').value = user_details.Address;
             document.getElementById('pin_code').value = user_details.pin_code;
             document.getElementById('phone_no').value = user_details.phone_number;
@@ -22,12 +23,14 @@ firebase.auth().onAuthStateChanged(function(user) {
 
         document.getElementById('wd_form_submit').onclick = function(){
             var name_wd = document.getElementById('name').value;
+            var l_name_wd = document.getElementById('l_name').value;
             var phn_wd = document.getElementById('phone_no').value;
             var address_wd = document.getElementById('address').value;
             var pincode_wd = document.getElementById('pin_code').value;
             
             eventRef.child(user.uid).child('details').update({
                 name: name_wd, 
+                l_name: l_name_wd,
                 phone_number: phn_wd,
                 Address: address_wd,
                 pin_code: pincode_wd
