50576032e7635fb635b712dc880ae25ad757da4a
Bhargav Borse <bhargavborse4898@gmail.com>
2019-01-31 12:05:35 +0530

    feedback part done

--- js_files/feedback.js
+++ js_files/feedback.js
@@ -1,22 +1,34 @@
-var userSettingRef = firebase.database().ref('users');
+var eventRef = firebase.database().ref('users');
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
+        // User is signed in.
+        // console.log(user.uid);
+        console.log(user.email);
+        document.getElementById('user_email').innerHTML = user.email;
+        document.getElementById('user_email_text').innerHTML = user.email;
         
-      // User is signed in.
-      var phone = document.getElementById('Phone');
-     
-      var description = document.getElementById('Description');
-      userSettingRef = userSettingRef.child(user.uid).child('details');
-      userSettingRef.on('value',function(snapshot_userdetails){
-            var userdetails = snapshot_userdetails.val();
-            // phone = userdetails.phone;
-            // description = userdetails.description;
-            // email = userdetails.email;
-            document.getElementById('Email').value = userdetails.email;
-      });
+        // this part is for auto fill. will be used in personal details
+        eventRef.child(user.uid).child('details').on('value',function(user_details_snapshot){
+            var user_details = user_details_snapshot.val();
+            document.getElementById('name').value = user_details.name;
+        });
+        // end of fetching personal details
     } else {
-      // No user is signed in.
-      alert('No user is logged in');
-      window.location = 'login.html';
+        // No user is signed in.
+        window.alert('Sorry! No user has been signed in. Please try logging in again');
+        window.location = 'Guest index.html';
     }
-  });
+});
+
+function logout_user(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        alert('Signout Successful');
+        window.location = 'Guest index.html';
+    }).catch(function(error) {
+        // An error happened.
+        alert('Please try again');
+    });
+    
+}
