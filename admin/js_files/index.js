a47e41fe5f6ffb6df2ffa0dc23c6df6d08f2e6a4
Bhargav Borse <bhargavborse4898@gmail.com>
2019-02-14 12:10:05 +0530

    js part of deliveryman added, forgot password of admin done

--- admin/js_files/index.js
+++ admin/js_files/index.js
@@ -1,3 +1,5 @@
-function logout(){
+function logout_user(){
     firebase.auth().signOut();
+    window.alert('Successfully Signed Out');
+    window.location = "login.html";
   }
--- admin/js_files/login.js
+++ admin/js_files/login.js
@@ -40,28 +40,12 @@ function login(){
   
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
-    var errorCode = error.code;
+    // var errorCode = error.code;
     var errorMessage = error.message;
     
     window.alert("Error : " + errorMessage);
-    window.alert("Error in code" + errorCode);
+    // window.alert("Error in code" + errorCode);
     // ...
-  });
-  
-}
-
-function create_account(){
-  
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    
-    window.alert("Error : " + errorMessage);
-    
   });
   
 }
@@ -76,7 +60,7 @@ function forgot_password(){
     window.alert("To reset password please check your email");
   }).catch(function(error) {
     // An error happened.
-    window.alert("Error");
+    window.alert("Please enter correct Email Id");
   });
   
 }
