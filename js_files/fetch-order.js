5c1e310eab92c4b853c877a96293f0610394f676
Bhargav Borse <bhargavborse4898@gmail.com>
2019-03-03 13:06:51 +0530

    fetching order for current users completed

---
+++ js_files/fetch-order.js
@@ -0,0 +1,91 @@
+var eventRef = firebase.database().ref('users');
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+
+        // var databaseRef = firebase.database().ref()('users');
+        // eventRef.on('value', function(snap){
+        //     var users = snap.val();
+        //     var keys = Object.keys(users);
+            
+        //     for(var i = 0; i< keys.length;i++){
+        //         console.log( 'st user key- '+ keys[0]);
+        //     }
+        // });
+        eventRef.child(user.uid).child('details').on('value',function(user_details_snapshot){
+            var user_details = user_details_snapshot.val();
+            document.getElementById('user_email').innerHTML = user_details.email;
+            document.getElementById('phone_no_text').innerHTML = user_details.phone_number;
+        });
+        // end of fetching personal details
+        // fetch data in order history table
+        var tableRef = document.getElementById('fetch_order').getElementsByTagName('tbody')[0];
+        // databaseRef.child(keys[i]).child('details').child('email').on('value',function(email)        
+        eventRef.child(user.uid).child('Order').on('child_added',function(Order_snapshot){
+            
+            var product_name = Order_snapshot.child('product_name').val();
+            var Quantity = Order_snapshot.child('Quantity').val();
+            var Price = Order_snapshot.child('Price').val();
+            var Ordered_Date = Order_snapshot.child('Ordered_Date').val();
+            var Order_Type = Order_snapshot.child('Order_Type').val(); 
+            var Order_Status = Order_snapshot.child('Order_Status').val();
+            var Delivery_Return_Date = Order_snapshot.child('Delivery_Return_Date').val();
+            var Address = Order_snapshot.child('Address').val();
+            
+            // Insert a row in the table at the last row
+            var newRow   = tableRef.insertRow(0);
+            
+            //Cells
+            var product_name_cell = newRow.insertCell(0);
+            var Quantity_cell = newRow.insertCell(1);
+            var Price_cell = newRow.insertCell(2);
+            var Ordered_Date_cell = newRow.insertCell(3);
+            var Order_Type_cell = newRow.insertCell(4);
+            var Order_Status_cell = newRow.insertCell(5);
+            var Delivery_Return_Date_cell = newRow.insertCell(6);
+            var Address_cell = newRow.insertCell(7);
+            
+            //CellValue
+            var product_name_cell_value = document.createTextNode(product_name);
+            var Quantity_cell_value = document.createTextNode(Quantity);
+            var Price_cell_value = document.createTextNode(Price);
+            var Ordered_Date_cell_value = document.createTextNode(Ordered_Date);
+            var Order_Type_cell_value = document.createTextNode(Order_Type);
+            var Order_Status_cell_value = document.createTextNode(Order_Status);
+            var Delivery_Return_Date_cell_value = document.createTextNode(Delivery_Return_Date);
+            var Address_cell_value = document.createTextNode(Address);
+            
+            product_name_cell.appendChild(product_name_cell_value);
+            Quantity_cell.appendChild(Quantity_cell_value);
+            Price_cell.appendChild(Price_cell_value);
+            Ordered_Date_cell.appendChild(Ordered_Date_cell_value);
+            Order_Type_cell.appendChild(Order_Type_cell_value);
+            Order_Status_cell.appendChild(Order_Status_cell_value);
+            Delivery_Return_Date_cell.appendChild(Delivery_Return_Date_cell_value);
+            Address_cell.appendChild(Address_cell_value);
+        });
+        
+        
+    } else {
+        // No user is signed in.
+        window.alert('Sorry! No user has been signed in. Please try logging in again');
+        window.location = 'Guest index.html';
+    }
+    
+});
+
+function logout_user(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        alert('Signout Successful');
+        window.location = 'Guest index.html';
+    }).catch(function(error) {
+        // An error happened.
+        alert('Please try again');
+    });
+    
+}
+
+
+
--- js_files/order.js
+++ js_files/order.js
@@ -65,33 +65,5 @@ function logout_user(){
     
 }
 
-var tableRef = document.getElementById('fetch_order').getElementsByTagName('tbody')[0];
-firebase.database().ref().child('users').child(user.uid).child('Order').on('child_added',function(feedback_snapshot){
-    
-    var feedback_email = feedback_snapshot.child('email').val();
-    var feedback_message = feedback_snapshot.child('mesage').val();
-    var feedback_name = feedback_snapshot.child('name').val();
-    
-    // Insert a row in the table at the last row
-    var newRow   = tableRef.insertRow(0);
-    
-    //Cells
-    var customer_name_cell = newRow.insertCell(0);
-    var email_cell = newRow.insertCell(1);
-    var message_cell = newRow.insertCell(2);
-    var customer_name_cell = newRow.insertCell(3);
-    var email_cell = newRow.insertCell(4);
-    var message_cell = newRow.insertCell(5);
-    var customer_name_cell = newRow.insertCell(6);
-    var email_cell = newRow.insertCell(7);
-    
-    //CellValue
-    var customer_name_cell_value = document.createTextNode(feedback_name);
-    var  email_cell_value = document.createTextNode(feedback_email);
-    var message_cell_value = document.createTextNode(feedback_message);
-    
-    customer_name_cell.appendChild(customer_name_cell_value);
-    email_cell.appendChild(email_cell_value);
-    message_cell.appendChild(message_cell_value);
-});
+
 
