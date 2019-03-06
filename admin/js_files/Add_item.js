ab5cdb1d1af4e50142074a505d5e4ce35c17cd91
Dhams7 <pateldharmesh007.dp@gmail.com>
2019-03-06 13:25:10 +0530

    add item done

--- admin/js_files/Add_item.js
+++ admin/js_files/Add_item.js
@@ -6,28 +6,36 @@ var database = firebase.database().ref();
 document.getElementById('btn_insertname').onclick = function(){
 
    
-    
+    alert('wsff');
    var item_id = document.getElementById('item_id').value;
    var item_name = document.getElementById('item_name').value;
    var item_description = document.getElementById('item_description').value;
-   var item_category = document.getElementById('item_category').value;
+    var item_category = document.getElementById('category');
+    var cat = item_category.options[item_category.selectedIndex].value;
+   var item_subcategory = document.getElementById('response_val');
+   var sub = item_subcategory.options[item_subcategory.selectedIndex].value;
+//    var deals_of_the_day = document.getElementById('dod').value;
+//    var recommanded = document.getElementById('recommanded').value;
    var item_quantity = document.getElementById('item_quantity').value;
    var item_price = document.getElementById('item_price').value;
-   var display_page = document.getElementById('display_page').value;
+   var item_size = document.getElementById('size').value;
    var item_image = document.getElementById('item_image').value;
    var optional_image = document.getElementById('optional_image').value;
 
     // var image = document.getElementById('image').value;
 
-    database.child('item_images').child('men').push({
+    database.child('item').push({
 
         item_id : item_id,
         item_name : item_name,
         item_description : item_description,
-        item_category : item_category,
+        item_category : cat,
+        item_subcategory : sub,
+        // deals_of_the_day : deals_of_the_day,
+        // recommanded : recommanded,
         item_quantity : item_quantity,
         item_price : item_price,
-        display_page : display_page,
+        item_size : item_size,
         item_image : item_image,
         optional_image : optional_image
 
