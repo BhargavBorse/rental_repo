0f77f793f6129837f2e51fec5f6c5d49f160fbcc
Bhargav Borse <bhargavborse4898@gmail.com>
2019-03-11 11:23:11 +0530

    image storage and database done

--- admin/js_files/Add_item.js
+++ admin/js_files/Add_item.js
@@ -1,8 +1,7 @@
-
-var database = firebase.database().ref();
-
-
-
+var databaseRef = firebase.database().ref();
+document.getElementById('fileButton').onchange = function(event){
+    selectedFile = event.target.files[0];
+}
 document.getElementById('btn_insertname').onclick = function(){
     
     var item_id = document.getElementById('item_id').value;
@@ -10,33 +9,40 @@ document.getElementById('btn_insertname').onclick = function(){
     var item_description = document.getElementById('item_description').value;
     var item_category = document.getElementById('category');
     var cat = item_category.options[item_category.selectedIndex].value;
-    var item_subcategory = document.getElementById('response_val');
-    var sub = item_subcategory.options[item_subcategory.selectedIndex].value;
+    // var item_subcategory = document.getElementById('response_val');
+    // var sub = item_subcategory.options[item_subcategory.selectedIndex].value;
     var deals_of_the_day = document.getElementById('dod').checked;
     var recommanded = document.getElementById('rec').checked;
     var item_quantity = document.getElementById('item_quantity').value;
     var item_price = document.getElementById('item_price').value;
     var item_size = document.getElementById('size').value;
-    var item_image = document.getElementById('item_image').value;
-    var optional_image = document.getElementById('optional_image').value;
+    //var item_image = document.getElementById('item_image').value;
+    //var optional_image = document.getElementById('fileButton').value;
     
-    // var image = document.getElementById('image').value;
-    
-    database.child('item').push({
+    var filename = selectedFile.name;
+    var storageRef = firebase.storage().ref('ClothingPictures/' + selectedFile.name);
+    var uploadTask = storageRef.put(selectedFile);
+    uploadTask.on('state_changed',
+    function(snapshot){
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        alert('Upload Progress : '+progress+'%');
+    },function(error){
         
-        item_id : item_id,
-        item_name : item_name,
-        item_description : item_description,
-        item_category : cat,
-        item_subcategory : sub,
-        deals_of_the_day : deals_of_the_day,
-        recommanded : recommanded,
-        item_quantity : item_quantity,
-        item_price : item_price,
-        item_size : item_size,
-        item_image : item_image,
-        optional_image : optional_image
+    },function(){
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL){
+            databaseRef.child('item').child(cat).push({
+                item_id : item_id,
+                item_name : item_name,
+                item_description : item_description,
+                deals_of_the_day : deals_of_the_day,
+                recommended : recommanded,
+                item_quantity : item_quantity,
+                item_price : item_price,
+                item_size : item_size,
+                optional_image : downloadURL
+            });
+        });
         
+        alert('Done! Uploading');
     });
-    alert('Added');
 };
