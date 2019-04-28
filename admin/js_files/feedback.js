f4a99103298e9328aa30540a20fa380ed53f6af5
Dhams7 <pateldharmesh007.dp@gmail.com>
2019-04-28 23:27:34 +0530

    feedback

--- admin/js_files/feedback.js
+++ admin/js_files/feedback.js
@@ -7,6 +7,9 @@ firebase.database().ref().child('Feedback').on('child_added',function(feedback_s
     var feedback_name = feedback_snapshot.child('name').val();
     var feedback_date = feedback_snapshot.child('feed_date').val();
     var feedback_time = feedback_snapshot.child('feed_time').val();
+    var feedback_message_for = feedback_snapshot.child('message_for').val();
+
+
 
     
     // Insert a row in the table at the last row
@@ -15,20 +18,23 @@ firebase.database().ref().child('Feedback').on('child_added',function(feedback_s
     //Cells
     var customer_name_cell = newRow.insertCell(0);
     var email_cell = newRow.insertCell(1);
-    var message_cell = newRow.insertCell(2);
-    var date_cell = newRow.insertCell(3);
-    var time_cell = newRow.insertCell(3);
+    var message_for_cell = newRow.insertCell(2);
+    var message_cell = newRow.insertCell(3);
+    var date_cell = newRow.insertCell(4);
+    var time_cell = newRow.insertCell(5);
 
     
     //CellValue
     var customer_name_cell_value = document.createTextNode(feedback_name);
     var  email_cell_value = document.createTextNode(feedback_email);
+    var message_for_cell_value = document.createTextNode(feedback_message_for);
     var message_cell_value = document.createTextNode(feedback_message);
     var date_cell_value = document.createTextNode(feedback_date);
     var time_cell_value = document.createTextNode(feedback_time);
     
     customer_name_cell.appendChild(customer_name_cell_value);
     email_cell.appendChild(email_cell_value);
+    message_for_cell.appendChild(message_for_cell_value);
     message_cell.appendChild(message_cell_value);
     date_cell.appendChild(date_cell_value);
     time_cell.appendChild(time_cell_value);
